401c68fed41f2285befe8a2169e112b52db31f58
Will Knez <wbknez.dev@gmail.com>
2018-11-17 22:27:56 -0700

    Add object that represents a single, final output pixel.

---
+++ src/main/kotlin/com/solsticesquared/kalahari/render/Pixel.kt
@@ -0,0 +1,21 @@
+package com.solsticesquared.kalahari.render
+
+/**
+ * Represents a single, drawable RGB (red, green, and blue) color produced
+ * from a ray tracing algorithm.
+ *
+ * Pixels represent the final stage of the rendering pipeline: visualization
+ * via output.  They contain the finalized color that is created from all
+ * individual processes such as sampling, lighting, post-processing, and gamma
+ * correction.  Because of this, pixels are only suitable for output and may
+ * not be used for any additional ray tracing computations.
+ *
+ * @property x
+ *           The x-axis coordinate in pixels.
+ * @property y
+ *           The y-axis coordinate in pixels.
+ * @property rgb
+ *           The RGB color value.
+ */
+data class Pixel(@JvmField val x: Int, @JvmField val y: Int,
+                 @JvmField val rgb: Int)
