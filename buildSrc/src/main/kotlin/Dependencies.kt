8d9f6ccfe1e564d2df243a9120529f6dac8234e5
Will Knez <wbknez.dev@gmail.com>
2018-11-18 00:26:02 -0700

    Update dependency versions and labels to be "const".

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -5,10 +5,10 @@ sealed class Versions {
     
     companion object {
         
-        val kotlin     = "1.3.10"
-        val kotlintest = "2.0.7"
-        val rxjava     = "2.2.3"
-        val rxkotlin   = "2.3.0"
+        const val kotlin     = "1.3.10"
+        const val kotlintest = "2.0.7"
+        const val rxjava     = "2.2.3"
+        const val rxkotlin   = "2.3.0"
     }
 }
 
@@ -20,10 +20,15 @@ sealed class Libraries {
 
     companion object {
 
-        val kotlin     = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
-        val kotlinref  = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
-        val kotlintest = "io.kotlintest:kotlintest:${Versions.kotlintest}"
-        val rxjava     = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
-        val rxkotlin   = "io.reactivex.rxjava2:rxkotlin:${Versions.rxkotlin}"
+        const val kotlin     =
+            "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+        const val kotlinref  =
+            "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
+        const val kotlintest =
+            "io.kotlintest:kotlintest:${Versions.kotlintest}"
+        const val rxjava     =
+            "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
+        const val rxkotlin   =
+            "io.reactivex.rxjava2:rxkotlin:${Versions.rxkotlin}"
     }
 }
