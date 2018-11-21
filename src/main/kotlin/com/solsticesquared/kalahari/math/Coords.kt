234cfafa6d37cc402d24668a32b462c1bea7021d
Will Knez <wbknez.dev@gmail.com>
2018-11-20 18:54:30 -0700

    Add object that represents a pair of immutable Cartesian coordinates.

---
+++ src/main/kotlin/com/solsticesquared/kalahari/math/Coords.kt
@@ -0,0 +1,12 @@
+package com.solsticesquared.kalahari.math
+
+/**
+ * Represents a single pair of immutable Cartesian coordinates in
+ * two-dimensional space.
+ *
+ * @property x
+ *        The x-axis coordinate.
+ * @property y
+ *        The y-axis coordinate.
+ */
+data class Coords(val x: Int, val y: Int)
