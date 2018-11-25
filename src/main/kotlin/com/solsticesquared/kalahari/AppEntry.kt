d68e70e3d5b05c67c0adb736d79ed33c4fabaf85
Will Knez <wbknez.dev@gmail.com>
2018-11-25 14:26:56 -0700

    Update application entry point to use/call Lua.

--- src/main/kotlin/com/solsticesquared/kalahari/AppEntry.kt
+++ src/main/kotlin/com/solsticesquared/kalahari/AppEntry.kt
@@ -2,6 +2,7 @@ package com.solsticesquared.kalahari
 
 import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.mainBody
+import org.luaj.vm2.lib.jse.JsePlatform
 
 /**
  * The main driver for the Kalahari ray tracing project.
@@ -13,6 +14,10 @@ sealed class AppEntry {
      * types of the values they accept.
      */
     private class Arguments(parser: ArgParser) {
+
+        val file by parser.storing(
+            "-f", "--file", help = "a Lua file that describes a scene"
+        )
     }
 
     companion object {
@@ -24,8 +29,12 @@ sealed class AppEntry {
          *        The array of command line arguments, if any.
          */
         @JvmStatic
-        fun main(args: Array<String>) = mainBody("kalahari") {
+        fun main(args: Array<String>): Unit = mainBody("kalahari") {
             ArgParser(args).parseInto(::Arguments).run {
+                val globals = JsePlatform.standardGlobals()
+                val chunk   = globals.loadfile(this.file)
+
+                chunk.call()
             }
         }
     }
