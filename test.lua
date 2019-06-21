5e334c0656b4265a31cb17824fb78155502d6aa5
Will Knez <wbknez.dev@gmail.com>
2019-06-21 10:33:21 -0600

    Fix test case(s).

--- test.lua
+++ test.lua
@@ -1,85 +1,57 @@
-local bounds = luajava.bindClass("com.solsticesquared.kalahari.math.Bounds")
-local color = luajava.bindClass("com.solsticesquared.kalahari.math.color.Color3")
-local image = luajava.bindClass("com.solsticesquared.kalahari.render.ImageOutput")
-local order = luajava.bindClass("com.solsticesquared.kalahari.render.order.NaturalDrawingOrder")
-local pipeline = luajava.bindClass("com.solsticesquared.kalahari.render.Pipeline")
-local point3 = luajava.bindClass("com.solsticesquared.kalahari.math.Point3")
-local sphere = luajava.bindClass("com.solsticesquared.kalahari.math.volume.BoundingSphere")
-local tracer = luajava.bindClass("com.solsticesquared.kalahari.render.Tracer")
+local bounds = khlua.bindClass("com.solsticesquared.kalahari.math.Bounds")
+local color = khlua.bindClass("com.solsticesquared.kalahari.math.color.Color3")
+local image = khlua.bindClass("com.solsticesquared.kalahari.render.ImageOutput")
+local order = khlua.bindClass("com.solsticesquared.kalahari.render.order.NaturalDrawingOrder")
+local pipeline = khlua.bindClass("com.solsticesquared.kalahari.render.Pipeline")
+local point3 = khlua.bindClass("com.solsticesquared.kalahari.math.Point3")
+local sphere = khlua.bindClass("com.solsticesquared.kalahari.math.volume.BoundingSphere")
+local tracer = khlua.bindClass("com.solsticesquared.kalahari.render.Tracer")
 
 -- Variables.
-local nord = luajava.newInstance(
-    "com.solsticesquared.kalahari.render.order.NaturalDrawingOrder"
-)
+local nord = khlua.newInstance("NaturalDrawingOrder")
 
-local out = luajava.newInstance(
-    "com.solsticesquared.kalahari.render.ImageOutput",
-    "test", image.Format.PNG
-)
+local pipe = khlua.newInstance("Pipeline")
 
-local pipe = luajava.newInstance(
-    "com.solsticesquared.kalahari.render.Pipeline"
-)
+local view = khlua.newInstance("Bounds", 500, 500)
 
-local view = luajava.newInstance(
-    "com.solsticesquared.kalahari.math.Bounds",
-    500, 500
-)
+local out = khlua.newInstance("ImageOutput", "test", image.Format.PNG)
 
 -- Colors
-local red = luajava.newInstance(
-    "com.solsticesquared.kalahari.math.color.Color3",
-    1.0, 0.0, 0.0
-)
+local red = khlua.newInstance("Color3", 1.0, 0.0, 0.0)
 
-local orange = luajava.newInstance(
-        "com.solsticesquared.kalahari.math.color.Color3",
-        1.0, 0.70, 0.0
-)
+local orange = khlua.newInstance("Color3", 1.0, 0.70, 0.0)
 
-local yellow = luajava.newInstance(
-        "com.solsticesquared.kalahari.math.color.Color3",
-        1.0, 1.0, 0.0
-)
+local yellow = khlua.newInstance("Color3", 1.0, 1.0, 0.0)
 
-local green = luajava.newInstance(
-        "com.solsticesquared.kalahari.math.color.Color3",
-        0.0, 0.70, 0.0
-)
+local green = khlua.newInstance("Color3", 0.0, 0.70, 0.0)
 
-local blue = luajava.newInstance(
-        "com.solsticesquared.kalahari.math.color.Color3",
-        0.0, 0.0, 1.0
-)
+local blue = khlua.newInstance("Color3", 0.0, 0.0, 1.0)
 
-local white = luajava.newInstance(
-        "com.solsticesquared.kalahari.math.color.Color3",
-        1.0, 1.0, 1.0
-)
+local white = khlua.newInstance("Color3", 1.0, 1.0, 1.0)
 
 -- Bounding spheres.
-local spRed = luajava.newInstance(
-        "com.solsticesquared.kalahari.math.volume.BoundingSphere",
-        101.0, 150.0, 150.0, 0.0
+local spRed = khlua.newInstance(
+    "com.solsticesquared.kalahari.math.volume.BoundingSphere",
+    101.0, 150.0, 150.0, 0.0
 )
 
-local spYellow = luajava.newInstance(
-        "com.solsticesquared.kalahari.math.volume.BoundingSphere",
-        101.0, 250.0, 250.0, 0.0
+local spYellow = khlua.newInstance(
+    "com.solsticesquared.kalahari.math.volume.BoundingSphere",
+    101.0, 250.0, 250.0, 0.0
 )
 
-local spBlue = luajava.newInstance(
-        "com.solsticesquared.kalahari.math.volume.BoundingSphere",
-        101.0, 350.0, 350.0, 0.0
+local spBlue = khlua.newInstance(
+    "com.solsticesquared.kalahari.math.volume.BoundingSphere",
+    101.0, 350.0, 350.0, 0.0
 )
 
-local trace_it = luajava.createProxy("com.solsticesquared.kalahari.render.Tracer", {
+local trace_it = khlua.createProxy("com.solsticesquared.kalahari.render.Tracer", {
     trace = function(coords)
         local x = coords:getX()
         local y = coords:getY()
-        local p = luajava.newInstance(
-                "com.solsticesquared.kalahari.math.Point3",
-                x, y, 0.0
+        local p = khlua.newInstance(
+            "com.solsticesquared.kalahari.math.Point3",
+            x, y, 0.0
         )
 
         -- Primary colors.
