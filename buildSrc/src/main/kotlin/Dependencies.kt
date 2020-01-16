c761cfd59d22ddf05d2b51fb39d449e8479958d5
Will Knez <wbknez.devgmail.com>
2020-01-16 13:15:58 -0700

    Add project dependencies.

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,35 @@
+/**
+ * Represents a collection of dependency versions for this project.
+ */
+object Versions {
+
+    const val klaxon     = "5.2"
+    const val kotlin     = "1.3.60"
+    const val kotlinargp = "2.0.7"
+    const val kotlintest = "3.4.2"
+    const val luaj       = "3.0.1"
+    const val rxjava     = "2.2.13"
+    const val rxkotlin   = "2.4.0"
+}
+
+/**
+ * Represents a collection of all fully-qualified dependencies with their
+ * versions used in this project in Gradle-compatible format.
+ */
+object Libraries {
+
+    const val klaxon      =
+        "com.beust:klaxon:${Versions.klaxon}"
+    const val kotlin      =
+        "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    const val kotlinref   =
+        "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
+    const val kotlinargp  =
+        "com.xenomachina:kotlin-argparser:${Versions.kotlinargp}"
+    const val kotlintest  =
+        "io.kotlintest:kotlintest-runner-junit5:${Versions.kotlintest}"
+    const val luaj        = "org.luaj:luaj-jse:${Versions.luaj}"
+    const val rxjava      = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
+    const val rxkotlin    =
+        "io.reactivex.rxjava2:rxkotlin:${Versions.rxkotlin}"
+}
