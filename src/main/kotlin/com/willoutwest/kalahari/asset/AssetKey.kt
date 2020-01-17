0bc546f4dedf83cea6304d0f2e3d104ca425caa9
Will Knez <wbknez.devgmail.com>
2020-01-17 02:55:30 -0700

    Add convenience constructor to automatically construct a Path from a String.

--- src/main/kotlin/com/willoutwest/kalahari/asset/AssetKey.kt
+++ src/main/kotlin/com/willoutwest/kalahari/asset/AssetKey.kt
@@ -1,6 +1,7 @@
 package com.willoutwest.kalahari.asset
 
 import java.nio.file.Path
+import java.nio.file.Paths
 
 /**
  * Represents a collection of unique identifying information about an asset.
@@ -11,6 +12,16 @@ import java.nio.file.Path
  *           The path to an input stream.
  */
 data class AssetKey(val id: String, val path: Path) : Cloneable {
+
+    /**
+     * Constructor.
+     *
+     * @param id
+     *        The unique identifier to use.
+     * @param filename
+     *        The file path to use.
+     */
+    constructor(id: String, filename: String) : this(id, Paths.get(filename))
 
     constructor(key: AssetKey?) : this(key!!.id, key.path)
 
