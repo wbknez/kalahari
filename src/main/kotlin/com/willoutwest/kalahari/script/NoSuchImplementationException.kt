9d1be53a75fa1fcb68128f033ae078941bbc810c
Will Knez <wbknez.devgmail.com>
2020-01-19 17:40:40 -0700

    Add exception that is thrown when a proxy implementation can not be found.

--- src/main/kotlin/com/willoutwest/kalahari/script/NoSuchImplementationException.kt
+++ src/main/kotlin/com/willoutwest/kalahari/script/NoSuchImplementationException.kt
@@ -4,7 +4,8 @@ import java.lang.RuntimeException
 
 /**
  * Represents an implementation of [RuntimeException] that is thrown when an
- * attempt is made to import a class or package into Lua that does not exist.
+ * attempt is made to use a method implementation for a proxy that does not
+ * exist.
  */
-class NoSuchImportException(msg: String, t: Throwable? = null)
+class NoSuchImplementationException(msg: String, t: Throwable? = null)
     : RuntimeException(msg, t)
