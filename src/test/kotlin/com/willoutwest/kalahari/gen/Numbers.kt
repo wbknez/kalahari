856f28f74e91c62331b34d0765233dd8c68c088d
Will Knez <wbknez.devgmail.com>
2020-01-21 13:26:21 -0700

    Add additional float generator for non-zero floats.

--- src/test/kotlin/com/willoutwest/kalahari/gen/Numbers.kt
+++ src/test/kotlin/com/willoutwest/kalahari/gen/Numbers.kt
@@ -4,6 +4,22 @@ import io.kotlintest.properties.Gen
 import kotlin.math.abs
 
 /**
+ * Produces a randomized sequence of negative numeric <code>float</code>
+ * values.
+ */
+fun Gen.Companion.negativeFloats(): Gen<Float> = numericFloats().map {
+    abs(it) * -1f
+}
+
+/**
+ * Produces a randomized sequence of numeric <code>float</code> values
+ * between one and zero, inclusive.
+ */
+fun Gen.Companion.nonzeroFloats(): Gen<Float> = numericFloats().filter {
+    it  != 0f
+}
+
+/**
  * Produces a randomized sequence of positive numeric <code>float</code>
  * values.
  */
@@ -12,9 +28,7 @@ fun Gen.Companion.positiveFloats(): Gen<Float> = numericFloats().map {
 }
 
 /**
- * Produces a randomized sequence of negative numeric <code>float</code>
- * values.
+ * Produces a randomized sequence of numeric <code>float</code> values
+ * between one and zero, inclusive.
  */
-fun Gen.Companion.negativeFloats(): Gen<Float> = numericFloats().map {
-    abs(it) * -1f
-}
+fun Gen.Companion.smallFloats(): Gen<Float> = numericFloats(0f, 1f)
