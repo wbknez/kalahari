db70775688e3642f80e31854b86d770049146215
Will Knez <wbknez.devgmail.com>
2020-01-22 19:19:04 -0700

    Add object that represents a bounded sphere.

---
+++ src/main/kotlin/com/willoutwest/kalahari/math/intersect/BoundingSphere.kt
@@ -0,0 +1,157 @@
+package com.willoutwest.kalahari.math.intersect
+
+import com.willoutwest.kalahari.math.MathUtils
+import com.willoutwest.kalahari.math.Point3
+import com.willoutwest.kalahari.math.Ray3
+import com.willoutwest.kalahari.util.hash
+import kotlin.math.pow
+
+/**
+ * Represents an implementation of [BoundingVolume] that is defined as a
+ * simple sphere.
+ *
+ * @property radius
+ *           The radius of the sphere.
+ */
+class BoundingSphere(val radius: Float = 1f,
+                     override val center: Point3 = Point3.Zero.clone())
+    : BoundingVolume, Cloneable {
+
+    override val max: Point3
+        get() = Point3(this.center.x + this.radius,
+                       this.center.y + this.radius,
+                       this.center.z + this.radius)
+
+    override val min: Point3
+        get() = Point3(this.center.x - this.radius,
+                       this.center.y - this.radius,
+                       this.center.z - this.radius)
+
+    /**
+     * Constructor.
+     *
+     * @param radius
+     *        The radius to use.
+     * @param x
+     *        The x-axis coordinate of the center to use.
+     * @param y
+     *        The y-axis coordinate of the center to use.
+     * @param z
+     *        The z-axis coordinate of the center to use.
+     */
+    constructor(radius: Float = 1f, x: Float = 0.0f, y: Float = 0.0f,
+                z: Float = 0.0f)
+        : this(radius, Point3(x, y, z))
+
+    /**
+     * Constructor.
+     *
+     * @param sphere
+     *        The bounding sphere to copy.
+     */
+    constructor(sphere: BoundingSphere?) : this(sphere!!.radius, sphere.center)
+
+    override fun clone(): BoundingSphere = BoundingSphere(this)
+
+    override fun contains(point: Point3): Boolean =
+        this.center.distanceSquaredTo(point) < this.radius.pow(2)
+
+    override fun equals(other: Any?): Boolean =
+        when(other) {
+            is BoundingSphere -> this.radius == other.radius &&
+                                 this.center == other.center
+            else              -> false
+        }
+
+    override fun hashCode(): Int = hash(this.radius, this.center)
+
+    override fun intersects(ray: Ray3): Boolean {
+        // The algorithm to model bounding sphere to ray intersections uses
+        // nothing more than basic algebra and vector analysis to solve the
+        // resulting quadratic equation obtained by inserting the equation of
+        // a ray into the equation of a sphere.
+        //
+        // Courtesy of:
+        //  [1] Paul Bourke (1992), "Intersection of a Line and a Sphere (or a
+        //  circle)".
+        //  [2] University of Maryland Baltimore County, "Ray-Sphere
+        //  Intersection".
+        //  [3] RMIT Australia (Haines), "Intersection Detection".
+        val dX = this.center.x - ray.origin.x
+        val dY = this.center.y - ray.origin.y
+        val dZ = this.center.z - ray.origin.z
+
+        val dDotD    = dX * ray.dir.x + dY * ray.dir.y +
+                       dZ * ray.dir.z
+        val dSquared = dX * dX + dY * dY + dZ * dZ
+        val rSquared = this.radius * this.radius
+
+        return !(dDotD < 0f && dSquared > rSquared) &&
+               (dSquared - (dDotD * dDotD)) <= rSquared
+    }
+
+    override fun intersects(volume: BoundingVolume): Boolean {
+        return volume.intersectsSphere(this)
+    }
+
+    override fun intersectsBox(box: BoundingBox): Boolean {
+        // The algorithm to model bounding sphere to bounding box
+        // intersections exploits simple distance computations to determine
+        // whether or not they overlap.
+        //
+        // In brief, a sphere and a box intersect if the distance from the
+        // center of the sphere to one of the extreme points on the box is
+        // less than or equal to the radius of the sphere.
+        //
+        // Courtesy of Jim Arvo, "Graphics Gems 2".
+        var dist = this.radius * this.radius
+        val maxX = box.center.x + box.xLen
+        val maxY = box.center.y + box.yLen
+        val maxZ = box.center.z + box.zLen
+        val minX = box.center.x - box.xLen
+        val minY = box.center.y - box.yLen
+        val minZ = box.center.z - box.zLen
+
+        if(this.center.x < minX) {
+            dist -= MathUtils.pow(this.center.x - minX, 2f)
+        }
+        else if(this.center.x > maxX) {
+            dist -= MathUtils.pow(this.center.x - maxX, 2f)
+        }
+
+        if(this.center.y < minY) {
+            dist -= MathUtils.pow(this.center.y - minY, 2f)
+        }
+        else if(this.center.y > maxY) {
+            dist -= MathUtils.pow(this.center.y - maxY, 2f)
+        }
+
+        if(this.center.z < minZ) {
+            dist -= MathUtils.pow(this.center.z - minZ, 2f)
+        }
+        else if(this.center.z > maxZ) {
+            dist -= MathUtils.pow(this.center.z - maxZ, 2f)
+        }
+
+        return dist >= 0f
+    }
+
+    override fun intersectsSphere(sphere: BoundingSphere): Boolean {
+        // The algorithm to model bounding sphere to bounding sphere
+        // intersections is very simple, which is why bounding spheres are so
+        // nice to work with.
+        //
+        // In brief, the spheres intersect if the distance between their
+        // centers is less than or equal to the sum of their combined radii.
+        // This can be shortened by using the squared distance and comparing
+        // to the squared radii; this adds a multiplication but spares a
+        // square root.
+        //
+        // Courtesy of Gamasutra, "Simple Intersection Tests for Games"
+        val dist = this.center.distanceSquaredTo(sphere.center)
+        val radii = this.radius + sphere.radius
+        return dist <= (radii * radii)
+    }
+
+    override fun toString(): String = "(${this.radius}, ${this.center})"
+}
