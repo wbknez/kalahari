ad3dcf68679d11382c7dd9925dc747a93a71658f
Will Knez <wbknez.devgmail.com>
2020-01-24 13:42:42 -0700

    Fix new pool capacity computation and add constructor requirements.

--- src/main/kotlin/com/willoutwest/kalahari/util/pool/FlexibleConstraintHandler.kt
+++ src/main/kotlin/com/willoutwest/kalahari/util/pool/FlexibleConstraintHandler.kt
@@ -12,8 +12,31 @@ package com.willoutwest.kalahari.util.pool
 class FlexibleConstraintHandler(val increment: Int = 0,
                                 val scale: Float = 2f) : ConstraintHandler {
 
-    override fun handleViolation(pool: ObjectPool<*>) {
-        pool.capacity = pool.capacity + this.increment +
-                        (pool.capacity * this.scale).toInt()
+    init {
+        require(this.increment >= 0) {
+            "Flexible increment must be greater than or equal to zero."
+        }
+
+        require(this.scale >= 0f) {
+            "Flexible scale must be greater than or equal to zero."
+        }
+
+        require((this.increment != 0) || (this.scale != 0f)) {
+            "Flexible constraint handler must have at least one of non-zero " +
+            "increment or scale."
+        }
     }
+
+    override fun handleViolation(pool: ObjectPool<*>) {
+        val computed = pool.capacity + this.increment +
+                       (pool.capacity * this.scale).toInt()
+
+        pool.capacity = when(computed != pool.capacity) {
+            false -> pool.capacity + 1
+            true  -> computed
+        }
+    }
+
+    override fun toString(): String = "(inc: ${this.increment}, " +
+                                      "scale: ${this.scale})"
 }
