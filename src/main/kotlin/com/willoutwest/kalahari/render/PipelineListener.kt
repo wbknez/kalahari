d2247f111978e72621e20edb55bf0a34f4edec0c
Will Knez <wbknez.devgmail.com>
2020-01-27 09:08:04 -0700

    Add mechanism for being notified of rendering events.

---
+++ src/main/kotlin/com/willoutwest/kalahari/render/PipelineListener.kt
@@ -0,0 +1,32 @@
+package com.willoutwest.kalahari.render
+
+/**
+ * Represents a mechanism for being notified of rendering events from a
+ * [Pipeline].
+ */
+interface PipelineListener {
+
+    /**
+     * Called whenever a [Pipeline] finishes rendering a scene.
+     */
+    fun onComplete()
+
+    /**
+     * Called whenever a single [Pixel] has been fully traced and is ready to
+     * be visualized or serialized in some way.
+     *
+     * @param pixel
+     *        The pixel that was emitted.
+     */
+    fun onEmit(pixel: Pixel)
+
+    /**
+     * Called before a [Pipeline] begins rendering a scene in order to ensure
+     * all listeners are aware of the shape of the rendering window (which also
+     * defines the total number of pixels to be drawn).
+     *
+     * @param bounds
+     *        The bounds of the rendering window to use.
+     */
+    fun onStart(bounds: Bounds)
+}
