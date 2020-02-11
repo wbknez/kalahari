39792894d38a6fae1b2495cc763df2c3ea07ba81
Will Knez <wbknez.devgmail.com>
2020-02-10 22:28:15 -0700

    Update viewport fields to be accessible from Lua.

--- src/main/kotlin/com/willoutwest/kalahari/scene/camera/Viewport.kt
+++ src/main/kotlin/com/willoutwest/kalahari/scene/camera/Viewport.kt
@@ -29,30 +29,17 @@ import com.willoutwest.kalahari.util.hash
  * @property sampler
  *           The sampling mechanism to reduce anti-aliasing.
  */
-class Viewport(var bgColor: Color3 = Color3(0f, 0f, 0f),
-               var bounds: Bounds = Bounds(
-                   0, 0, 600, 400),
-               gamma: Float = 1f,
-               var gamutOp: ColorOperator = ClampingColorOperator(),
-               var maxDepth: Int = 1,
-               var pixelSize: Float = 1f,
-               var sampler: Sampler2 = Sampler.squareOf(
-                   1, 83, JitteredSampleGenerator()
-               )) : Cloneable {
-
-    var gamma: Float    = 1f
-        set(value) {
-            require(value != 0f) {
-                "Gamma correction cannot be zero."
-            }
-
-            this.invGamma = 1f / value
-
-            field = value
-        }
-
-    var invGamma: Float = 1f
-        private set
+data class Viewport(@JvmField var bgColor: Color3 = Color3(0f, 0f, 0f),
+                    @JvmField var bounds: Bounds = Bounds(600, 400),
+                    @JvmField var gamma: Float = 1f,
+                    @JvmField var gamutOp: ColorOperator =
+                        ClampingColorOperator(),
+                    @JvmField var invGamma: Float = 1f,
+                    @JvmField var maxDepth: Int = 1,
+                    @JvmField var pixelSize: Float = 1f,
+                    @JvmField var sampler: Sampler2 = Sampler.squareOf(
+                       1, 83, JitteredSampleGenerator()
+                    )) : Cloneable {
 
     /**
      * Constructor.
@@ -62,31 +49,8 @@ class Viewport(var bgColor: Color3 = Color3(0f, 0f, 0f),
      */
     constructor(viewport: Viewport?) :
         this(viewport!!.bgColor, viewport.bounds, viewport.gamma,
-             viewport.gamutOp, viewport.maxDepth, viewport.pixelSize,
-             viewport.sampler)
+             viewport.gamutOp, viewport.invGamma, viewport.maxDepth,
+             viewport.pixelSize, viewport.sampler)
 
-    init {
-        this.gamma = gamma
-    }
-
-    public override fun clone(): Viewport =
-        Viewport(this)
-
-    override fun equals(other: Any?): Boolean =
-        when(other) {
-            is Viewport -> this.bgColor == other.bgColor &&
-                           this.bounds == other.bounds &&
-                           this.gamma == other.gamma &&
-                           this.gamutOp == other.gamutOp &&
-                           this.invGamma == other.invGamma &&
-                           this.maxDepth == other.maxDepth &&
-                           this.pixelSize == other.pixelSize &&
-                           this.sampler == other.sampler
-            else           -> false
-        }
-
-    override fun hashCode(): Int = hash(this.bgColor, this.bounds,
-                                        this.gamma, this.gamutOp,
-                                        this.maxDepth, this.pixelSize,
-                                        this.sampler)
+    public override fun clone(): Viewport = Viewport(this)
 }
