d0b201da3469eb1c7be590341625e8c29beac261
Will Knez <wbknez.devgmail.com>
2020-02-10 22:09:52 -0700

    Add skeleton for Camera.

---
+++ src/main/kotlin/com/willoutwest/kalahari/scene/camera/Camera.kt
@@ -0,0 +1,8 @@
+package com.willoutwest.kalahari.scene.camera
+
+/**
+ * Represents a collection of viewing parameters that control how pairs of
+ * pixel coordinates are transformed into rays that trace the boundaries of a
+ * scene.
+ */
+class Camera
