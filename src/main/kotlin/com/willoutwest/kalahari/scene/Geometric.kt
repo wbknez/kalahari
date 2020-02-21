00add653a9f010a49c92f89b06ec92cba7559b98
Will Knez <wbknez.devgmail.com>
2020-02-20 19:20:20 -0700

    Add object that represents a geometric surface scenegraph element.

---
+++ src/main/kotlin/com/willoutwest/kalahari/scene/Geometric.kt
@@ -0,0 +1,39 @@
+package com.willoutwest.kalahari.scene
+
+import com.willoutwest.kalahari.math.EpsilonTable
+import com.willoutwest.kalahari.math.Ray3
+import com.willoutwest.kalahari.math.intersect.Intersection
+import com.willoutwest.kalahari.util.FloatContainer
+
+/**
+ * Represents an implementation of [Actor] that contains an arbtirary
+ * geometric surface with associated visualization information.
+ */
+class Geometric(name: String, private val surface: Surface)
+    : AbstractActor(name), Actor, Cloneable {
+
+    /**
+     * Constructor.
+     *
+     * @param geom
+     *        The geometric actor to copy from.
+     */
+    constructor(geom: Geometric?) : this(geom!!.name, geom.surface)
+
+    override fun clone(): Geometric = Geometric(this)
+
+    override fun intersect(ray: Ray3, tMin: FloatContainer,
+                           record: Intersection, eps: EpsilonTable): Boolean {
+        if(this.bounds?.intersects(ray) == false) {
+            return false
+        }
+
+        return when(this.surface.intersect(ray, tMin, record, eps)) {
+            false -> false
+            true  -> {
+                record.obj = this
+                true
+            }
+        }
+    }
+}
