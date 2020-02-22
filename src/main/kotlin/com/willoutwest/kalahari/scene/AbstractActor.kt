6049e1062c7c21efabacb634ab907a679901f9e9
Will Knez <wbknez.devgmail.com>
2020-02-22 13:47:49 -0700

    Add implementations of actor movement.

--- src/main/kotlin/com/willoutwest/kalahari/scene/AbstractActor.kt
+++ src/main/kotlin/com/willoutwest/kalahari/scene/AbstractActor.kt
@@ -1,5 +1,8 @@
 package com.willoutwest.kalahari.scene
 
+import com.willoutwest.kalahari.math.CacheUtils
+import com.willoutwest.kalahari.math.Quaternion
+import com.willoutwest.kalahari.math.Vector3
 import com.willoutwest.kalahari.math.intersect.BoundingVolume
 
 /**
@@ -12,5 +15,67 @@ abstract class AbstractActor(override val name: String) : Actor {
 
     override var enabled: Boolean = true
 
+    override val motion: Motion = Motion()
+
     override var parent: Actor? = null
+
+    override fun move(x: Float, y: Float, z: Float): Actor {
+        this.motion.translation.x += x
+        this.motion.translation.y += y
+        this.motion.translation.z += z
+
+        return this
+    }
+
+    override fun move(vec: Vector3): Actor {
+        this.motion.translation.plusSelf(vec)
+
+        return this
+    }
+
+    override fun rotate(angle: Float, axis: Vector3): Actor {
+        val cache = CacheUtils.localCache
+        val quat  = cache.quats.borrow()
+
+        quat.setFromAxis(angle, axis)
+        this.motion.rotation.timesSelf(quat)
+
+        cache.quats.reuse(quat)
+
+        return this
+    }
+
+    override fun rotate(roll: Float, pitch: Float, yaw: Float): Actor {
+        val cache = CacheUtils.localCache
+        val quat  = cache.quats.borrow()
+
+        quat.setFromEuler(roll, pitch, yaw)
+        this.motion.rotation.timesSelf(quat)
+
+        cache.quats.reuse(quat)
+
+        return this
+    }
+
+    override fun rotate(quat: Quaternion): Actor {
+        this.motion.rotation.timesSelf(quat)
+
+        return this
+    }
+
+    override fun scale(x: Float, y: Float, z: Float): Actor {
+        this.motion.scale.x *= x
+        this.motion.scale.y *= y
+        this.motion.scale.z *= z
+
+        return this
+    }
+
+    override fun scale(vec: Vector3): Actor {
+        this.motion.scale.x *= vec.x
+        this.motion.scale.y *= vec.y
+        this.motion.scale.z *= vec.z
+
+        return this
+    }
 }
