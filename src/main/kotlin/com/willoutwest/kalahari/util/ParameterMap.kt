9f9d6ff814e268ce69ec78dbca1f27a82b7bfee4
Will Knez <wbknez.devgmail.com>
2020-03-03 16:33:54 -0700

    Add object that represents a collection of parameters associated by name.

---
+++ src/main/kotlin/com/willoutwest/kalahari/util/ParameterMap.kt
@@ -0,0 +1,46 @@
+package com.willoutwest.kalahari.util
+
+/**
+ * Represents a collection of parameter values, associated by name, that serve
+ * as the basis for ray tracing related objects such as cameras, lights, and
+ * materials.
+ *
+ * In previous versions of this project, this class utilized a hash map
+ * directly and provided numerous typecasting functions.  In this version,
+ * however, Kotlin property delegate obviate this need and allow seamless
+ * optional field access with a built-in API accessible from Lua scripts via
+ * generated per property get/set methods.
+ *
+ * @property id
+ *           The unique identifier; usually context specific.
+ * @property params
+ *           The collection of parameter values associated by name; these
+ *           are typically accessed through property delegation.
+ */
+open class ParameterMap(val id: String) : Cloneable {
+
+    protected val params = mutableMapOf<String, Any>()
+
+    /**
+     * Constructor.
+     *
+     * @param map
+     *        The parameter map to copy from.
+     */
+    constructor(map: ParameterMap?) : this(map!!.id) {
+        this.params.putAll(map.params)
+    }
+
+    public override fun clone(): ParameterMap = ParameterMap(this)
+
+    override fun equals(other: Any?): Boolean =
+        when(other) {
+            is ParameterMap -> this.id == other.id &&
+                               this.params == other.params
+            else            -> false
+        }
+
+    override fun hashCode(): Int = hash(this.id, this.params)
+
+    override fun toString() = "ParameterMap(${this.id}, ${this.params})"
+}
