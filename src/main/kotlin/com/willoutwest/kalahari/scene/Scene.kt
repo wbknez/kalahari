a9cb3aa4d54be92e1a1600350bf3e355fd46cb70
Will Knez <wbknez.devgmail.com>
2020-03-08 10:16:26 -0700

    Add camera to Scene.

--- src/main/kotlin/com/willoutwest/kalahari/scene/Scene.kt
+++ src/main/kotlin/com/willoutwest/kalahari/scene/Scene.kt
@@ -1,17 +1,23 @@
 package com.willoutwest.kalahari.scene
 
 import com.willoutwest.kalahari.math.intersect.Intersectable
+import com.willoutwest.kalahari.scene.camera.Camera
 import com.willoutwest.kalahari.scene.camera.Viewport
 
 /**
  * Represents a ray traceable scene.
  *
- * @property viewport
- *           The viewing plane.
+ * @property camera
+ *           The camera.
  * @property root
  *           The root element of the scenegraph.
+ * @property viewport
+ *           The viewing plane.
  */
 class Scene {
+
+    @JvmField
+    var camera: Camera = Camera("camera")
 
     @JvmField
     var root: Group = Group("root")
