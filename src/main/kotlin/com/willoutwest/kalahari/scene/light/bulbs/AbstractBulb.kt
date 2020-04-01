bc641debf08149e2c6ac0a56fb459d39f5bfd584
Will Knez <wbknez.devgmail.com>
2020-04-01 16:44:54 -0700

    Add abstract implementation of Bulb.

---
+++ src/main/kotlin/com/willoutwest/kalahari/scene/light/bulbs/AbstractBulb.kt
@@ -0,0 +1,21 @@
+package com.willoutwest.kalahari.scene.light.bulbs
+
+import com.willoutwest.kalahari.math.Vector3
+import com.willoutwest.kalahari.math.intersect.Intersection
+import com.willoutwest.kalahari.scene.light.Bulb
+import com.willoutwest.kalahari.scene.light.Light
+
+/**
+ * Represents an implementation of [Bulb] that provides a default
+ * implementation for deriving classes.
+ */
+abstract class AbstractBulb : Bulb {
+
+    override fun G(light: Light, record: Intersection): Float = 1f
+
+    override fun pdf(light: Light, record: Intersection): Float = 1f
+
+    override fun shineOn(light: Light, record: Intersection,
+                         store: Vector3): Vector3 =
+        store.set(Vector3.Zero)
+}
