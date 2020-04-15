a32a1183b7657cae60da0bdb4a9e1387041eb79f
Will Knez <wbknez.dev@gmail.com>
2020-04-14 18:45:46 -0700

    Add ambient lighting and light list to Scene.

--- src/main/kotlin/com/willoutwest/kalahari/scene/Scene.kt
+++ src/main/kotlin/com/willoutwest/kalahari/scene/Scene.kt
@@ -1,14 +1,18 @@
 package com.willoutwest.kalahari.scene
 
-import com.willoutwest.kalahari.math.intersect.Intersectable
 import com.willoutwest.kalahari.scene.camera.Camera
 import com.willoutwest.kalahari.scene.camera.Viewport
+import com.willoutwest.kalahari.scene.light.Light
 
 /**
  * Represents a ray traceable scene.
  *
+ * @property ambient
+ *           The ambient light, if any.
  * @property camera
  *           The camera.
+ * @property lights
+ *           The collection of active lights.
  * @property root
  *           The root element of the scenegraph.
  * @property viewport
@@ -17,7 +21,13 @@ import com.willoutwest.kalahari.scene.camera.Viewport
 class Scene {
 
     @JvmField
+    var ambient: Light? = null
+
+    @JvmField
     var camera: Camera = Camera("camera")
+
+    @JvmField
+    var lights: MutableList<Light> = mutableListOf()
 
     @JvmField
     var root: Group = Group("root")
