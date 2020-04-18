40e49367a7bdb929a98dd335b77602f23e64d356
Will Knez <wbknez.dev@gmail.com>
2020-04-18 12:56:10 -0700

    Add object that represents a collection of rendering parameters.

---
+++ src/main/kotlin/com/willoutwest/kalahari/material/Material.kt
@@ -0,0 +1,61 @@
+package com.willoutwest.kalahari.material
+
+import com.willoutwest.kalahari.math.sample.Sampler3
+import com.willoutwest.kalahari.util.ParameterMap
+import com.willoutwest.kalahari.util.Typeable
+import com.willoutwest.kalahari.util.hash
+
+/**
+ * Represents a collection of rendering parameters that controls the
+ * colorization of reflected light from an arbitrary geometric surface.
+ *
+ * @property cD
+ *        The ambient and diffuse reflection color.
+ * @property kA
+ *        The ambient reflection coefficient.
+ * @property kD
+ *           The diffuse reflection coefficient.
+ * @property sampler
+ *
+ */
+class Material(name: String) :
+    Cloneable, ParameterMap(name), Typeable<Material.Type> {
+
+    /**
+     * Represents a unique identifier that denotes the type of shader that
+     * should be used to determine the color of light reflected from a
+     * material.
+     */
+    interface Type
+
+    var cD: ColorSource by this.params
+
+    var kA: Float by this.params
+
+    var kD: Float by this.params
+
+    var sampler: Sampler3 by this.params
+
+    override var type: Type by this.params
+
+    /**
+     * Constructor.
+     *
+     * @param material
+     *        The material to copy from.
+     */
+    constructor(material: Material?) : this(material!!.id) {
+        this.params.putAll(material.params)
+    }
+
+    override fun clone(): Material = Material(this)
+
+    override fun equals(other: Any?): Boolean =
+        when(other) {
+            is Material -> super.equals(other) &&
+                           this.type == other.type
+            else        -> false
+        }
+
+    override fun hashCode(): Int = hash(super.hashCode(), this.type)
+}
