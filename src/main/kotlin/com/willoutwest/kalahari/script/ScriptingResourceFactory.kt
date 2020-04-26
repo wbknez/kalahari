e7270551d4684125ff95c9c3facc1a5ea6341c78
Will Knez <wbknez.dev@gmail.com>
2020-04-25 17:05:22 -0700

    Add marker interface for scripting resources.

---
+++ src/main/kotlin/com/willoutwest/kalahari/script/ScriptingResourceFactory.kt
@@ -0,0 +1,10 @@
+package com.willoutwest.kalahari.script
+
+/**
+ * Designates a producer of a scene resource such as materials, lights, or
+ * various geometries.
+ *
+ * This is a marker interface that denotes classes intended to be included
+ * in the scripting library as a sub-module via tables.
+ */
+interface ScriptingResourceFactory
