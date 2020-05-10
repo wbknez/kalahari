4ea5809d54c0a0a16bae477d5b61baf91f56e19b
Will Knez <wbknez.dev@gmail.com>
2020-05-09 17:54:31 -0700

    Update ShadowMode to only be enable/disable for casting/receiving split.

--- src/main/kotlin/com/willoutwest/kalahari/scene/shadow/ShadowMode.kt
+++ src/main/kotlin/com/willoutwest/kalahari/scene/shadow/ShadowMode.kt
@@ -1,35 +1,19 @@
 package com.willoutwest.kalahari.scene.shadow
 
 /**
- * Represents a mechanism for specifying how physical objects  in a
- * scene may cast or receive shadows onto or from others.
+ * Represents a mechanism for specifying how physical objects in a
+ * scene respond to shadowing operations.
  */
 enum class ShadowMode {
 
     /**
-     * Represents an object that may both cast shadows on and receive shadows
-     * from other objects.
-     *
-     * This is the default shadow mode for most actors and other objects.
-     */
-    CastAndReceive,
-
-    /**
-     * Represents an object that may only cast shadows on other objects;
-     * shadow rays that intersect its geometry will be ignored and not visible.
-     */
-    Cast,
-
-    /**
-     * Represents an object that may neither cast on other objects nor be
-     * affected by those shadows that fall upon it.
+     * Represents an object that is not affected by a specific shadowing
+     * operation.
      */
     Disable,
 
     /**
-     * Represents an object that may only receive shadows from other objects;
-     * any kind of shadow that originates from its own geometry will not be
-     * visible.
+     * Represents an object that is affected by a specific shadowing operation.
      */
-    Receive,
+    Enable
 }
