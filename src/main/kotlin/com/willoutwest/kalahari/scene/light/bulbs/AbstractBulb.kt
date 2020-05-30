0754f728ab97c3715a71334fad0f6238faa4ab55
Will Knez <wbknez.dev@gmail.com>
2020-05-30 11:27:36 -0700

    Add additional abstract method implementation(s).

--- src/main/kotlin/com/willoutwest/kalahari/scene/light/bulbs/AbstractBulb.kt
+++ src/main/kotlin/com/willoutwest/kalahari/scene/light/bulbs/AbstractBulb.kt
@@ -1,8 +1,10 @@
 package com.willoutwest.kalahari.scene.light.bulbs
 
+import com.willoutwest.kalahari.math.Color3
 import com.willoutwest.kalahari.math.Ray3
 import com.willoutwest.kalahari.math.Vector3
 import com.willoutwest.kalahari.math.intersect.Intersection
+import com.willoutwest.kalahari.scene.Actor
 import com.willoutwest.kalahari.scene.light.Bulb
 import com.willoutwest.kalahari.scene.light.Light
 
@@ -17,6 +19,11 @@ abstract class AbstractBulb : Bulb {
     override fun illuminate(light: Light, record: Intersection,
                             store: Vector3): Vector3 =
         store.set(Vector3.Zero)
+
+    override fun L(light: Light, root: Actor,
+                   record: Intersection, store: Color3): Color3 =
+        light.cL.getColor(record, store)
+            .timesSelf(light.kL)
 
     override fun pdf(light: Light, record: Intersection): Float = 1f
 
