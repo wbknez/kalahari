05c47fc39ba55e2b93188681b86007835cac0e6c
Will Knez <wbknez.dev@gmail.com>
2020-05-30 19:42:53 -0700

    Add object that represents an collection of color data loaded from disk.

---
+++ src/main/kotlin/com/willoutwest/kalahari/texture/Image.kt
@@ -0,0 +1,34 @@
+package com.willoutwest.kalahari.texture
+
+import com.willoutwest.kalahari.math.Color3
+
+/**
+ * Represents a collection of raw color data read from disk.
+ *
+ * @property data
+ *           The collection of image data.  This uses a stride of three to
+ *           denote the color components of each individual pixel.
+ * @property height
+ *           The image height in pixels.
+ * @property width
+ *           The image width in pixels.
+ */
+data class Image(val width: Int, val height: Int,
+                 private val data: List<Color3>) {
+
+    init {
+        require(height >= 1) {
+            "The image height must be positive."
+        }
+
+        require(width >= 1) {
+            "The image width must be positive."
+        }
+
+        require(data.size == (width * height)) {
+            "The amount of image data must equal the width times the height."
+        }
+    }
+
+    operator fun get(x: Int, y: Int): Color3 = this.data[x + this.width * y]
+}
