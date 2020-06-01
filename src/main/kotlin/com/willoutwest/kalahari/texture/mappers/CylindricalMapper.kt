6a20b170e63f2760b39e1855ee61992b3443e284
Will Knez <wbknez.dev@gmail.com>
2020-05-31 18:35:04 -0700

    Add object that represents a cylindrical image mapper.

--- src/main/kotlin/com/willoutwest/kalahari/texture/mappers/CylindricalMapper.kt
+++ src/main/kotlin/com/willoutwest/kalahari/texture/mappers/CylindricalMapper.kt
@@ -4,25 +4,23 @@ import com.willoutwest.kalahari.math.MathUtils
 import com.willoutwest.kalahari.math.Point3
 import com.willoutwest.kalahari.math.TexCoord2
 import com.willoutwest.kalahari.texture.ImageMapper
-import kotlin.math.acos
 import kotlin.math.atan2
 
 /**
  * Represents an implementation of [ImageMapper] that maps an intersection
- * point on a geometric surface onto a sphere in texture space.
+ * point on a geometric surface onto a cylinder in texture space.
  */
-class SphericalMapper : ImageMapper {
+class CylindricalMapper : ImageMapper {
 
-    override fun map(hit: Point3, width: Int, height: Int, store: TexCoord2)
-        : TexCoord2 {
-        val phi   = atan2(hit.x, hit.z)
-        val theta = acos(hit.y)
+    override fun map(hit: Point3, width: Int, height: Int,
+                     store: TexCoord2): TexCoord2 {
+        val phi = atan2(hit.x, hit.z)
 
         store.u = when(phi < 0.0) {
             false -> phi / MathUtils.InvTwoPi
-            true  -> 1f + (phi / MathUtils.InvTwoPi)
+            true  -> 1 + (phi / MathUtils.InvTwoPi)
         }
-        store.v = 1f - theta * MathUtils.InvPi
+        store.v = (hit.y + 1) * 0.5f
 
         return store
     }
