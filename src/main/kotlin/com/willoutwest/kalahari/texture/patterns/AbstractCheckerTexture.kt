bd866e82b997f1f2269267ab92063ec31c5c3d4c
Will Knez <wbknez.dev@gmail.com>
2020-06-07 21:53:36 -0700

    Add abstract implementation of a checker pattern.

---
+++ src/main/kotlin/com/willoutwest/kalahari/texture/patterns/AbstractCheckerTexture.kt
@@ -0,0 +1,17 @@
+package com.willoutwest.kalahari.texture.patterns
+
+import com.willoutwest.kalahari.texture.Texture
+
+/**
+ * Represents a basic collection of properties for creating a checker pattern.
+ *
+ * @property evenColor
+ *           The first checker color.
+ * @property oddColor
+ *           The second checker color.
+ * @property scale
+ *           The scaling factor for each checker.
+ */
+abstract class AbstractCheckerTexture(val evenColor: Texture,
+                                      val oddColor: Texture,
+                                      val scale: Float) : Texture
