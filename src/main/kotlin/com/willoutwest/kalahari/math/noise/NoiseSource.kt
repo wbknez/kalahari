2862d788008108bafd4853bfc670003696042945
Will Knez <wbknez.dev@gmail.com>
2020-06-12 21:35:28 -0700

    Update comments.

--- src/main/kotlin/com/willoutwest/kalahari/math/noise/NoiseSource.kt
+++ src/main/kotlin/com/willoutwest/kalahari/math/noise/NoiseSource.kt
@@ -3,6 +3,9 @@ package com.willoutwest.kalahari.math.noise
 
 /**
  * Represents a source, or creator, of noise.
+ *
+ * Noise sources are expected to be terminal and may not contain other
+ * [Speaker] objects (i.e. may not be nested further).
  */
 interface NoiseSource : Cloneable, Speaker {
 
