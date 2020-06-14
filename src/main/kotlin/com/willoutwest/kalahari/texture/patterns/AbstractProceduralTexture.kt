2094073358df32de39fc99209ecafd55f20d35fc
Will Knez <wbknez.dev@gmail.com>
2020-06-14 16:08:33 -0700

    Add generic skeleton for procedural textures.

---
+++ src/main/kotlin/com/willoutwest/kalahari/texture/patterns/AbstractProceduralTexture.kt
@@ -0,0 +1,19 @@
+package com.willoutwest.kalahari.texture.patterns
+
+import com.willoutwest.kalahari.math.noise.Speaker
+import com.willoutwest.kalahari.texture.AbstractTexture
+import com.willoutwest.kalahari.texture.Texture
+
+/**
+ * Represents an implementation of [Texture] that provides the
+ * infrastructure to create procedural textures using one or more noise
+ * outputs.
+ *
+ * @property speaker
+ *           The speaker to use for noise output.
+ * @property color
+ *           The color to use.
+ */
+abstract class AbstractProceduralTexture(val speaker: Speaker,
+                                         val color: Texture) :
+    AbstractTexture(), Texture
